1860044d61eca194ec73d26a4f820a6bc734401f
Alan <yewulove@qq.com>
2020-05-06 19:42:45 +0800

    Update common.py

--- model/common.py
+++ model/common.py
@@ -5,6 +5,17 @@ import torch.nn.functional as F
 import numpy as np 
 from torch.autograd import Variable, Function
 
+class MeanShift(nn.Conv2d):
+    def __init__(
+        self, rgb_range,
+        rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
+
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        std = torch.Tensor(rgb_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
+        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
+        for p in self.parameters():
+            p.requires_grad = False
 
 class Shuffle_d(nn.Module):
     def __init__(self, scale=2):
